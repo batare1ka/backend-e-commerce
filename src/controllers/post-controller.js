e447bae48ff15a3ba24c020445456dc7c3b4527b
Andrei ARPENTI <arsenalarsehavin23@mail.ru>
2026-07-26 22:01:18 +0300

    post-controller.js

---
+++ src/controllers/post-controller.js
@@ -0,0 +1,14 @@
+import { DiContainer } from '../container.js'
+
+export const postController = async (req, res, next) => {
+  try {
+    const pool = DiContainer.get('pool')
+    const result = await pool.query(`SELECT * FROM posts`)
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+      code: error.code,
+    })
+  }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import userRoutes from '../routes/userRoutes.js';
+import postRoutes from '../routes/postRoutes.js';
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 export default router;
