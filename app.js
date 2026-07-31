624b45d7d273429b937f06736a7194844b9b151a
Andrei ARPENTI <arsenalarsehavin23@mail.ru>
2026-07-31 10:27:26 +0300

    install dotenv

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import dotenv from "dotenv";
+
 
 import { allowCors } from './src/middlewares/cors.js'
 import { connectDb } from './src/config/config.js'
 import routes from './src/routes/index.js'
 import authRoutes from './src/routes/authRoutes.js';
+
+dotenv.config();
 
 const app = express()
 
